124461bae29ca4b17661a0b167a07579aafa9a5d
Truong Nguyen <tnguyen@demystdata.com>
2021-04-04 14:09:20 +1000

    add reminer

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import Discord from 'discord.js'
 import dotenv from 'dotenv';
+dotenv.config();
+
+import Discord from 'discord.js'
 import commands from './commands';
 import express from 'express';
 
-dotenv.config();
 
 (async () => {
   const client = new Discord.Client();
   const defaultChannelId = '827125469521379391';
+  let defaultChannel;
 
   client.on("message", async (message) => {
     for await (const cmd of commands) {
@@ -21,15 +23,19 @@ dotenv.config();
     }
   });
   client.on('ready', async () => {
-    const channel: any = await client.channels.cache.find(x => x.id === defaultChannelId);
-    channel.send('Hello, I am your crypto bot. glad to help you here.')
+    defaultChannel = await client.channels.cache.find(x => x.id === defaultChannelId);
+    defaultChannel.send('Hello, I am your crypto bot. glad to help you here.')
   })
   client.login(process.env.BOT_TOKEN);
   const app = express()
 
   app.get('/', (req, res) => {
-    res.send('I am the crypto post')
+    res.send('Hey, I am awaked')
   })
+  app.get('/reminder', (req, res) => {
+    defaultChannel.send('Hello Boss. kindy reminder you to activate your StomGain :)')
+  })
+
   const port = process.env.PORT || 5000;
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
