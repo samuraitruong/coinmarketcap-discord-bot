8c39ca44dc02adac8a213cf1e72d8aaf29937bcd
Truong Nguyen <tnguyen@demystdata.com>
2021-05-16 15:10:54 +1000

    allow empty period for chart command

--- src/commands/chart_command.ts
+++ src/commands/chart_command.ts
@@ -8,7 +8,7 @@ export class ChartCommand extends CommandBase {
 
   }
   async handleInput(input: string) {
-    const [cmd, coin, period] = input.split(' ').filter(Boolean).map(x => x.trim());
+    const [cmd, coin, period = ''] = input.split(' ').filter(Boolean).map(x => x.trim());
     const getMessage = (p: string) => {
       const message = new MessageEmbed();
       message.setImage(`http://coinspot-chart.herokuapp.com/chart_v1?period=${p}&coin=${coin}&lineWidth=1.5`)
